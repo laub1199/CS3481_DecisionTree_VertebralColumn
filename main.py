b01dc5614ba73d05fa325f19b2e6edf699252b97
Sennett Lau <laub1199@gmail.com>
2021-02-24 21:34:14 +0800

    decision tree with simple traning and detection

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
+import pandas as pd
+from sklearn import tree
+from sklearn import preprocessing
+import graphviz
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def decisionTree():
+    filepath = "./data/column_3C.dat"
+    names = ["pelvic_incidence", "pelvic_tilt", "lumbar_lordosis_angle", "sacral_slope", "pelvic_radius",
+             "degree_spondylolisthesis", "class"]
 
+    df = pd.read_csv(filepath, sep=" ", header=None, names=names)
+    df = df.sample(frac=1)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    numOfRow = len(df.index)
 
+    trainingIndex = numOfRow * 0.8
+    trainingIndex = int(trainingIndex)
 
-# Press the green button in the gutter to run the script.
+    training_data = df.iloc[0:trainingIndex, 0:6]
+    training_class = df.iloc[0:trainingIndex, -1]
+
+    le = preprocessing.LabelEncoder()
+    label = le.fit_transform(training_class)
+
+    # clf = tree.DecisionTreeClassifier(criterion='entropy')
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(training_data, label)
+
+    dot_data = tree.export_graphviz(clf, out_file=None, filled=True,
+                                    feature_names=["pelvic_incidence", "pelvic_tilt", "lumbar_lordosis_angle",
+                                                   "sacral_slope", "pelvic_radius", "degree_spondylolisthesis"],
+                                    class_names=["Hernia", "Spondylolisthesis", "Normal"], rounded=True,
+                                    special_characters=True)
+
+    graph_training = graphviz.Source(dot_data)
+    # graph_training.render('Graph', view=True)
+
+    testing_data = df.iloc[trainingIndex:numOfRow + 1, 0:6]
+    testing_class = df.iloc[trainingIndex:numOfRow + 1, -1]
+
+    clf = clf.predict(testing_data)
+
+    numberOfTestcase = numOfRow - trainingIndex
+    numberOfMathes = 0
+    for i in range(0, numberOfTestcase):
+        clfToName = ""
+        if (str(clf[i]) == "0"):
+            clfToName = "DH"
+        elif (str(clf[i]) == "1"):
+            clfToName = "NO"
+        else:
+            clfToName = "SL"
+        if (testing_class.iloc[i] == clfToName):
+            numberOfMathes += 1
+
+    print(str(numberOfMathes) + "/" + str(numberOfTestcase))
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    decisionTree()
