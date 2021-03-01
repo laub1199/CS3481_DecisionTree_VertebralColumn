085e0df5175828e38cf7ff38f22d367a9f201d52
Sennett Lau <laub1199@gmail.com>
2021-03-01 22:07:14 +0800

    added const shuffle, training and test error float list

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import pandas as pd
 from sklearn import tree
 from sklearn import preprocessing
+from sklearn.utils import shuffle
+import numpy as np
 import graphviz
 
-def decisionTree():
+def decisionTree(max_depth):
     filepath = "./data/column_3C.dat"
     names = ["pelvic_incidence", "pelvic_tilt", "lumbar_lordosis_angle", "sacral_slope", "pelvic_radius",
              "degree_spondylolisthesis", "class"]
 
     df = pd.read_csv(filepath, sep=" ", header=None, names=names)
-    df = df.sample(frac=1)
+    df = df.sample(n=len(df), random_state=42).reset_index(drop=True)
+    print(df)
 
     numOfRow = len(df.index)
 
-    trainingIndex = numOfRow * 0.8
+    trainingIndex = numOfRow * 0.7
     trainingIndex = int(trainingIndex)
 
     training_data = df.iloc[0:trainingIndex, 0:6]
@@ -23,7 +26,7 @@ def decisionTree():
     label = le.fit_transform(training_class)
 
     # clf = tree.DecisionTreeClassifier(criterion='entropy')
-    clf = tree.DecisionTreeClassifier()
+    clf = tree.DecisionTreeClassifier(max_depth=max_depth)
     clf = clf.fit(training_data, label)
 
     dot_data = tree.export_graphviz(clf, out_file=None, filled=True,
@@ -35,18 +38,33 @@ def decisionTree():
     graph_training = graphviz.Source(dot_data)
     # graph_training.render('Graph', view=True)
 
+    clf_train = clf.predict(training_data)
+
+    numberOfTrainingMathes = 0
+
+    for i in range(0, trainingIndex):
+        clfToName = ""
+        if (str(clf_train[i]) == "0"):
+            clfToName = "DH"
+        elif (str(clf_train[i]) == "1"):
+            clfToName = "NO"
+        else:
+            clfToName = "SL"
+        if (training_class.iloc[i] == clfToName):
+            numberOfTrainingMathes += 1
+
     testing_data = df.iloc[trainingIndex:numOfRow + 1, 0:6]
     testing_class = df.iloc[trainingIndex:numOfRow + 1, -1]
 
-    clf = clf.predict(testing_data)
+    clf_test = clf.predict(testing_data)
 
     numberOfTestcase = numOfRow - trainingIndex
     numberOfMathes = 0
     for i in range(0, numberOfTestcase):
         clfToName = ""
-        if (str(clf[i]) == "0"):
+        if (str(clf_test[i]) == "0"):
             clfToName = "DH"
-        elif (str(clf[i]) == "1"):
+        elif (str(clf_test[i]) == "1"):
             clfToName = "NO"
         else:
             clfToName = "SL"
@@ -54,6 +72,14 @@ def decisionTree():
             numberOfMathes += 1
 
     print(str(numberOfMathes) + "/" + str(numberOfTestcase))
+    return 1-(numberOfTrainingMathes/trainingIndex), 1-(numberOfMathes/numberOfTestcase)
 
 if __name__ == '__main__':
-    decisionTree()
+    train_list = []
+    test_list = []
+    for max_depth in range (1, 13):
+        training_error, test_error = decisionTree(max_depth)
+        train_list.append(training_error)
+        test_list.append(test_error)
+    print(train_list)
+    print(test_list)
