5b327dc9c8b915dc7a51cce3692b27c0cc1ee3dd
Sennett Lau <laub1199@gmail.com>
2021-03-19 02:47:02 +0800

    progress

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ import numpy as np
 import graphviz
 import matplotlib.pyplot as plt
 
-def decisionTree(max_depth, outputTree, target_depth):
-    # filepath = "./data/column_3C.dat"
-    filepath = "./out_noindex.csv"
+def decisionTree(max_leaf_nodes, outputTree, target_depth,RS):
+    filepath = "./data/column_3C.dat"
+    # filepath = "./out_noindex.csv"
     names = ["pelvic_incidence", "pelvic_tilt", "lumbar_lordosis_angle", "sacral_slope", "pelvic_radius",
              "degree_spondylolisthesis", "class"]
 
-    df = pd.read_csv(filepath, sep=",", header=None, names=names)
-    # df = df.sample(n=len(df), random_state=42)
+    df = pd.read_csv(filepath, sep=" ", header=None, names=names)
+    df = df.sample(n=len(df), random_state=RS)
+    # df = df.sample(n=len(df))
     #
     # compression_opts = dict(method='zip', archive_name='out.csv')
     # df.to_csv('out_index.zip', index=True, compression=compression_opts)
@@ -30,13 +31,12 @@ def decisionTree(max_depth, outputTree, target_depth):
 
     training_data = df.iloc[0:trainingIndex, 0:6]
     training_class = df.iloc[0:trainingIndex, -1]
-    print(training_data)
 
     le = preprocessing.LabelEncoder()
     label = le.fit_transform(training_class)
 
     # clf = tree.DecisionTreeClassifier(criterion='entropy')
-    clf = tree.DecisionTreeClassifier(max_depth=max_depth)
+    clf = tree.DecisionTreeClassifier(max_leaf_nodes=max_leaf_nodes, criterion='entropy')
     clf = clf.fit(training_data, label)
 
     if (outputTree):
@@ -47,11 +47,11 @@ def decisionTree(max_depth, outputTree, target_depth):
                                         special_characters=True)
 
         graph_training = graphviz.Source(dot_data)
-        graph_training.render('Graph'+str(max_depth), view=True)
+        graph_training.render('Graph'+str(max_depth), view=False)
 
     clf_train = clf.predict(training_data)
     a = 1-accuracy_score(label, clf_train)
-    if (max_depth == target_depth):
+    if (max_leaf_nodes == target_depth):
         print(1-accuracy_score(label, clf_train))
         print(confusion_matrix(label, clf_train))
 
@@ -59,12 +59,12 @@ def decisionTree(max_depth, outputTree, target_depth):
     testing_data = df.iloc[trainingIndex:numOfRow + 1, 0:6]
     testing_class = df.iloc[trainingIndex:numOfRow + 1, -1]
 
-    print(testing_data)
+    # print(testing_data)
 
     clf_test = clf.predict(testing_data)
     testing_label = le.fit_transform(testing_class)
 
-    if (max_depth == target_depth):
+    if (max_leaf_nodes == target_depth):
         print(1-accuracy_score(testing_label, clf_test))
         print(confusion_matrix(testing_label, clf_test))
 
@@ -75,20 +75,23 @@ def decisionTree(max_depth, outputTree, target_depth):
 
 
 if __name__ == '__main__':
-    train_list = []
-    test_list = []
-    levels = []
-    for max_depth in range(1, 14):
-        training_error, test_error = decisionTree(max_depth, True, 4)
-        train_list.append(training_error)
-        test_list.append(test_error)
-        levels.append(max_depth)
-    print(train_list)
-    print(test_list)
-    plt.plot(levels, train_list, label="Training Error")
-    plt.plot(levels, test_list, label="Testing Error")
-    plt.title("Training and Test Error Rate")
-    plt.xlabel("Size of Tree")
-    plt.ylabel("Error Rate")
-    plt.legend()
-    plt.show()
+    for RS in range(5, 10):
+        train_list = []
+        test_list = []
+        levels = []
+        for max_depth in range(2, 25):
+            training_error, test_error = decisionTree(max_depth, True, 6, RS)
+            train_list.append(training_error)
+            test_list.append(test_error)
+            levels.append(max_depth)
+        print(train_list)
+        print(test_list)
+        plt.plot(levels, train_list, label="Training Error")
+        plt.plot(levels, test_list, label="Testing Error")
+        plt.title("Training and Test Error Rate")
+        plt.xlabel("Size of Tree")
+        plt.ylabel("Error Rate")
+        plt.legend()
+        plt.show(block=False)
+        plt.pause(1)
+        plt.close()
